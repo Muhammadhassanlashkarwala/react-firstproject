f45fd227a7485010593dbdee67f90ff5ff341798
Muhammadhassanlashkarwala <bhaihassan036@gmail.com>
2024-09-09 03:09:43 +0500

    push

--- src/header.jsx
+++ src/header.jsx
@@ -8,8 +8,8 @@ function Header() {
             <nav>
                 <ul className="header-ul">
                     <li className="header-li"><a href="#home">Home</a></li>
-                    <li className="header-li"><a href="#about">About</a></li>
                     <li className="header-li"><a href="#service">Service</a></li>
+                    <li className="header-li"><a href="#about">About</a></li>
                     <li className="header-li"><a href="#contact">Contact</a></li>
                 </ul>
             </nav>
